39eae5b4a1ef7e7a0529216dd4c40d2619f161a8
aamir.khan <aamir.khan@aq-solutions.de>
2025-08-25 13:59:55 +0200

    runs.py has been modified to be compatible with docker

--- nnunet/runs.py
+++ nnunet/runs.py
@@ -1,53 +1,21 @@
-import sys
 import os
-sys.path.append(os.path.dirname(os.getcwd()))
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from nnunet.paths import my_output_identifier
 
-########################################################################################################################
-#                                              Experiment  preparing                                                   #
-########################################################################################################################
-home_dir = os.path.expanduser('~')
-#home_dir = "C:\\Users\\akh"
-train_dir = os.path.join(home_dir,'3D_Slicer','CTPelvic1K', 'inference_weights','nnUNet','rawdata','Task11_CTPelvic1K')
+# Paths
+home_dir = '/mnt/inference_weights'
+inference_weights_dir = home_dir
+train_dir = os.path.join(inference_weights_dir, 'rawdata', 'Task11_CTPelvic1K')
+output_dir = os.path.join(inference_weights_dir, 'nnUNet_raw', 'Task11_CTPelvic1K')
+processed_path = os.path.join(inference_weights_dir, 'nnUNet_processed', 'Task11_CTPelvic1K')
+check_save_path = os.path.join(inference_weights_dir, 'nnUNet_processed', 'Task11_CTPelvic1K', 'Task11_check')
 
-output_dir = os.path.join(home_dir,'3D_Slicer','CTPelvic1K', 'inference_weights','nnUNet','nnUNet_raw','Task11_CTPelvic1K')
-#command = f'python dataset_conversion\JstPelvisSegmentation_5label.py --train_dir {train_dir} --output_dir {output_dir}'
-#command = 'python experiment_planning\plan_and_preprocess_task.py -t Task11_CTPelvic1K -pl 8 -pf 8'
-
-processed_path = os.path.join(home_dir, '3D_Slicer','CTPelvic1K', 'inference_weights','nnUNet','nnUNet_processed','Task11_CTPelvic1K')
-check_save_path = os.path.join(home_dir, '3D_Slicer','CTPelvic1K', 'inference_weights','nnUNet','nnUNet_processed','Task11_CTPelvic1K','Task11_check')
-#command = f'python preprocessing\lumbosacral_joint_sampling.py --processed_path {processed_path} --check_save_path {check_save_path}'
-
-
-########################################################################################################################
-#                                              Experiment  running                                                     #
-########################################################################################################################
-#TASK = 'Task11_CTPelvic1K'
 TASK = 'Task22_ipcai2021'
 FOLD = 0
 GPU = 0
-"""
-    Training
-"""
-#command = f'python run/run_training.py 2d nnUNetTrainer {TASK} {FOLD} --gpu {GPU}' # TASK fold gpu_idx
-#command = f'python run/run_training.py 3d_fullres nnUNetTrainer {TASK} {FOLD} --gpu {GPU}'
-#command = f'python run/run_training.py 3d_lowres nnUNetTrainer {TASK} {FOLD} --gpu {GPU}'
-# command = f'python run/run_training.py 3d_cascade_fullres nnUNetTrainerCascadeFullRes {TASK} {FOLD} --gpu {GPU}'
 
-
-"""
-    Validation:  add " --validation_only" "--valbest"
-        pay attention: do_mirroring has been changed to "False"
-"""
-# command = f'python run/run_training.py 2d nnUNetTrainer {TASK} {FOLD} --gpu {GPU} --validation_only --valbest'
-# command = f'python run/run_training.py 3d_fullres nnUNetTrainer {TASK} {FOLD} --gpu {GPU} --validation_only --valbest'
-# command = f'python run/run_training.py 3d_lowres nnUNetTrainer {TASK} {FOLD} --gpu {GPU} --validation_only --valbest'
-# command = f'python run/run_training.py 3d_cascade_fullres nnUNetTrainerCascadeFullRes {TASK} {FOLD} --gpu {GPU} --validation_only --valbest'
-
-"""
-    Testing on data never seen
-"""
-test_data_path = os.path.join(home_dir,'3D_Slicer','CTPelvic1K', 'inference_weights','nnUNet','rawdata','ipcai2021_ALL_Test')
+test_data_path = os.path.join(inference_weights_dir, 'rawdata', 'ipcai2021_ALL_Test')
 
 command = f'python inference/predict_simple.py ' \
           f'-i {test_data_path} ' \
@@ -56,51 +24,51 @@ command = f'python inference/predict_simple.py ' \
           f'-tr nnUNetTrainer ' \
           f'-m 2d ' \
           f'-f {FOLD} ' \
-          f'--num_threads_preprocessing 8 '\
-          f'--num_threads_nifti_save 4 '\
+          f'--num_threads_preprocessing 8 ' \
+          f'--num_threads_nifti_save 4 ' \
           f'--gpu {GPU}'
-#
-#command = f'python inference/predict_simple.py ' \
-#           f'-i {test_data_path} ' \
-#           f'-o {test_data_path}/{TASK}__{my_output_identifier}__fold{FOLD}_3dfullres_pred ' \
-#           f'-t {TASK} ' \
-#           f'-tr nnUNetTrainer ' \
-#           f'-m 3d_fullres ' \
-#           f'-f {FOLD} ' \
-#           f'--gpu {GPU}'
-
-#
-# command = f'python inference/predict_simple.py ' \
-#           f'-i {test_data_path} ' \
-#           f'-o {test_data_path}/{TASK}__{my_output_identifier}__fold{FOLD}_3dlowres_pred ' \
-#           f'-t {TASK} ' \
-#           f'-tr nnUNetTrainer ' \
-#           f'-m 3d_lowres ' \
-#           f'-f {FOLD} ' \
-#           f'--gpu {GPU} ' \
-#           f'--overwrite_existing 0'
-#
-# my_task_lowres = TASK
-# my_output_identifier_lowres = 'CTPelvic1K' #your low_res experiment\'s "my_output_identifier" in path
-# command = f'python inference/predict_simple.py ' \
-#           f'-i {test_data_path} ' \
-#           f'-o {test_data_path}/{TASK}__{my_output_identifier_lowres}__{my_output_identifier}__fold{FOLD}_3dcascadefullres_pred ' \
-#           f'-t {TASK} ' \
-#           f'-tr nnUNetTrainerCascadeFullRes ' \
-#           f'-m 3d_cascade_fullres ' \
-#           f'-f {FOLD} ' \
-#           f'-l {test_data_path}/{my_task_lowres}__{my_output_identifier_lowres}__fold{FOLD}_3dlowres_pred ' \
-#           f'--gpu {GPU} ' \
-#           f'--overwrite_existing 0'
-
-########################################################################################################################
-#                                              evaluation                                                              #
-########################################################################################################################
-# command = 'python ../evaluation.py'
-# command = 'python ../save_evaluation_results2csv.py'
-# command = 'python ../save_evaluation_results2csv_Manu.py'
 
 if __name__ == '__main__':
     print('\n'*2)
-    print(command,'\n'*2)
+    print(command, '\n'*2)
     os.system(command)
+
+
+
+
+# import sys
+# import os
+
+# # Add the parent directory of 'nnunet' to the Python path
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from nnunet.paths import my_output_identifier
+
+# # Paths
+# home_dir = '/app'  # Use /app as the base directory in the container
+# inference_weights_dir = os.path.join(home_dir, 'inference_weights','nnUNet')
+# train_dir = os.path.join(inference_weights_dir, 'rawdata', 'Task11_CTPelvic1K')
+# output_dir = os.path.join(inference_weights_dir, 'nnUNet_raw', 'Task11_CTPelvic1K')
+# processed_path = os.path.join(inference_weights_dir, 'nnUNet_processed', 'Task11_CTPelvic1K')
+# check_save_path = os.path.join(inference_weights_dir, 'nnUNet_processed', 'Task11_CTPelvic1K', 'Task11_check')
+
+# TASK = 'Task22_ipcai2021'
+# FOLD = 0
+# GPU = 0
+
+# test_data_path = os.path.join(inference_weights_dir, 'rawdata', 'ipcai2021_ALL_Test')
+
+# command = f'python inference/predict_simple.py ' \
+#           f'-i {test_data_path} ' \
+#           f'-o {test_data_path}/{TASK}__{my_output_identifier}__fold{FOLD}_2d_pred ' \
+#           f'-t {TASK} ' \
+#           f'-tr nnUNetTrainer ' \
+#           f'-m 2d ' \
+#           f'-f {FOLD} ' \
+#           f'--num_threads_preprocessing 8 ' \
+#           f'--num_threads_nifti_save 4 ' \
+#           f'--gpu {GPU}'
+
+# if __name__ == '__main__':
+#     print('\n'*2)
+#     print(command, '\n'*2)
+#     os.system(command)
